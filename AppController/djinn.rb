79b2d2f1ccc8ea95ea86f85bdaa580e7c4187059
DrOctogon <tyler@appscale.com>
2014-01-22 15:52:33 -0800

    removed reporting number of appservers at login node

--- AppController/djinn.rb
+++ AppController/djinn.rb
@@ -911,7 +911,7 @@ class Djinn
     Current State: #{stats['state']}
     STATUS
 
-    if my_node.is_login?
+    if my_node.is_appengine?
       app_names = []
       stats['apps'].each { |k, v|
         app_names << k
